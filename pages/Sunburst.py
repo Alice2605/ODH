ed2fe16170bc828cbecbab49465d765ca923c5c3
Alice <77941134+Alice2605@users.noreply.github.com>
2023-12-15 14:59:33 +0100

    Update Sunburst.py

--- pages/Sunburst.py
+++ pages/Sunburst.py
@@ -9,41 +9,46 @@ from donnees import *
 
 st.markdown("# Sunburst diagram")
 
+uploaded_file = st.file_uploader("Ajoutez votre fichier Excel") # Espace pour ajouter le fichier excel qui sera traité
 
-# Liste des options du menu déroulant
-options = ['Sélectionner', 'CGFL', 'TOUT']
+if uploaded_file is not None: # Une fois qu'un fichier est importé
 
-# Affichage du menu déroulant dans l'application Streamlit
-selected_option = st.selectbox('Sélectionnez une option', options)
+    df = pd.read_excel(uploaded_file) # DataFrame à partir du fichier
 
+    # Liste des options du menu déroulant
+    options = ['Sélectionner', 'CGFL', 'TOUT']
 
-if selected_option == 'CGFL':
-    _, fig_cgfl_sunburst = donnees_cgfl(df)
-    fig_cgfl_sunburst.update_layout(#title_text="Patients ayant reçu du trastuzumab au CGFL", 
-        #title_font_size=20,
-        template="plotly_white",
-                    
-        margin={"t": 0, "r": 0, "b": 0, "l": 0},
-        width=1800,  # Largeur du graphique
-        height=1100  # Hauteur du graphique
-        ) # Change la taille de police du TITRE
-    
-    fig_cgfl_sunburst.update_traces(textfont_size=20) # Change la taille de police du texte des NOEUDS
-    st.plotly_chart(fig_cgfl_sunburst)
+    # Affichage du menu déroulant dans l'application Streamlit
+    selected_option = st.selectbox('Sélectionnez une option', options)
 
 
+    if selected_option == 'CGFL':
+        _, fig_cgfl_sunburst = donnees_cgfl(df)
+        fig_cgfl_sunburst.update_layout(#title_text="Patients ayant reçu du trastuzumab au CGFL", 
+            #title_font_size=20,
+            template="plotly_white",
+                        
+            margin={"t": 0, "r": 0, "b": 0, "l": 0},
+            width=1800,  # Largeur du graphique
+            height=1100  # Hauteur du graphique
+            ) # Change la taille de police du TITRE
+        
+        fig_cgfl_sunburst.update_traces(textfont_size=20) # Change la taille de police du texte des NOEUDS
+        st.plotly_chart(fig_cgfl_sunburst)
 
-if selected_option == 'TOUT':
-    _, fig_tout_sunburst = donnees_tout(df)
-    fig_tout_sunburst.update_layout(#title_text="Patients ayant reçu du trastuzumab (tous centres)", 
-        #title_font_size=20,
-        template="plotly_white",
-                    
-        margin={"t": 0, "r": 0, "b": 0, "l": 0},
-        width=1800,  # Largeur du graphique
-        height=1100  # Hauteur du graphique
-        ) # Change la taille de police du TITRE
-    
-    fig_tout_sunburst.update_traces(textfont_size=20) # Change la taille de police du texte des NOEUDS
-    st.plotly_chart(fig_tout_sunburst)
+
+
+    if selected_option == 'TOUT':
+        _, fig_tout_sunburst = donnees_tout(df)
+        fig_tout_sunburst.update_layout(#title_text="Patients ayant reçu du trastuzumab (tous centres)", 
+            #title_font_size=20,
+            template="plotly_white",
+                        
+            margin={"t": 0, "r": 0, "b": 0, "l": 0},
+            width=1800,  # Largeur du graphique
+            height=1100  # Hauteur du graphique
+            ) # Change la taille de police du TITRE
+        
+        fig_tout_sunburst.update_traces(textfont_size=20) # Change la taille de police du texte des NOEUDS
+        st.plotly_chart(fig_tout_sunburst)
 
