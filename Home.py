ff2dac81267343b06b1d46026869dfe3b5c7260c
Alice <77941134+Alice2605@users.noreply.github.com>
2023-12-19 17:30:10 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,39 @@
+import streamlit as st
+st.set_page_config(layout="wide", page_title="Home - Import Excel") # Configuration de la page
+
+import pandas as pd
+from functions import *
+from donnees import *
+
+
+
+
+
+
+
+st.markdown("# Home")
+
+
+
+
+uploaded_file = st.file_uploader("Ajoutez votre fichier Excel") # Espace pour ajouter le fichier excel qui sera traité
+
+if uploaded_file is not None: # Une fois qu'un fichier est importé
+
+    df = pd.read_excel(uploaded_file) # DataFrame à partir du fichier
+
+    # Liste des options du menu déroulant
+    options = ['Sélectionner', 'CGFL', 'TOUT']
+
+    # Affichage du menu déroulant dans l'application Streamlit
+    selected_option = st.selectbox('Sélectionnez une option', options)
+
+
+
+
+
+
+
+
+
 
