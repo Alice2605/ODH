e65b46333a39fc5b9359d7aa25f87e56268c19b5
Alice <77941134+Alice2605@users.noreply.github.com>
2023-12-21 16:23:32 +0100

    Update Sankey.py

--- pages/Sankey.py
+++ pages/Sankey.py
@@ -5,38 +5,51 @@ import pandas as pd
 from functions import *
 from donnees import *
 
+
 df = st.session_state.df
 
-st.markdown(f"<h1 style='text-align: center;'>Diagrammes de Sankey ODH, médicaments injectables par protocole</h1>", unsafe_allow_html=True) # Titre
+# Liste des centres
+liste_hopitaux = sorted(df["ID_HOPITAL"].unique().tolist())
+
+st.title("Diagrammes de Sankey") # Titre
 
 
 # Liste des options du menu déroulant
-options = ['Sélectionner', 'CGFL', 'TOUT']
+options = ['Sélectionner', 'TOUT'] + liste_hopitaux
 
 # Affichage du menu déroulant dans l'application Streamlit
-selected_option = st.selectbox('Sélectionnez une option', options)
+selected_option = st.selectbox('Sélectionnez un centre', options)
 
 
-# Si CGFL sélectionné dans le menu déroulant
-if selected_option == 'CGFL':
-    fig_cgfl_sankey, _ = donnees_cgfl(df) # , _ for sunburst
+# Si un centre est sélectionné dans le menu déroulant
+if selected_option in liste_hopitaux:
+    fig_centre_sankey, _ = donnees_centre(df, selected_option) 
 
-    fig_cgfl_sankey.update_layout(title_text="Patients ayant reçu du trastuzumab au CGFL", 
-                    title_font_size=30,
-                    width=1700  # Largeur du graphique
-                    ) # Change la taille de police du TITRE
-    fig_cgfl_sankey.update_traces(textfont_size=12) # Change la taille de police du texte des NOEUDS
-    st.plotly_chart(fig_cgfl_sankey) # Affiche le graphique
+    fig_centre_sankey.update_layout(
+        #title_text="Patients ayant reçu du trastuzumab au centre", 
+        #title_font_size=30,
+        width=1600  # Largeur du graphique
+        ) # Change la taille de police du TITRE
+    fig_centre_sankey.update_traces(textfont_size=12) # Change la taille de police du texte des NOEUDS
+    st.plotly_chart(fig_centre_sankey) # Affiche le graphique
+
+    st.download_button(label="Télécharger le graph au format html", data=fig_centre_sankey.to_html(full_html=False), file_name="Sankey_diagram.html")
 
 
 # Si TOUT sélectionné dans le menu déroulant (tous les centres)
 if selected_option == 'TOUT':
     fig_tout_sankey, _ = donnees_tout(df)
 
-    fig_tout_sankey.update_layout(title_text="Tous les patients ayant reçu du trastuzumab", 
-                    title_font_size=30, 
-                    width=1700  # Largeur du graphique
-                    ) # Change la taille de police du TITRE
+    fig_tout_sankey.update_layout(
+        title_text="Tous les patients ayant reçu du trastuzumab", 
+        title_font_size=30, 
+        width=1600  # Largeur du graphique
+        ) # Change la taille de police du TITRE
     fig_tout_sankey.update_traces(textfont_size=12) # Change la taille de police du texte des NOEUDS
     st.plotly_chart(fig_tout_sankey) # Affiche le graphique
+
+    st.download_button(label="Télécharger le graph au format html", data=fig_tout_sankey.to_html(full_html=False), file_name="Sankey_diagram_tout.html")
+
+
+
 
