5f886286bb5beb9faad484e5ba7f151048d89da5
Alice <77941134+Alice2605@users.noreply.github.com>
2023-12-21 16:21:34 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,14 +5,11 @@ import pandas as pd
 from functions import *
 from donnees import *
 
-st.markdown("# Home - Importation du fichier Excel")
+st.title("Home - Importation du fichier Excel")
 
-"""
-
-Choisissez le fichier Excel à importer, attendez que ça finisse de charger ("RUNNING" en haut à droite de la page) et vous pourrez changer de page pour voir les diagrammes
-
-"""
-
+st.markdown('#### Choisissez le fichier Excel à importer')
+st.markdown('##### :warning: **Attendez que ça finisse de charger** (barre bleue ci-dessous et "RUNNING" en haut à droite de la page) puis vous pourrez changer de page pour voir les diagrammes.')
+st.write('\n')
 
 uploaded_file = st.file_uploader("Ajoutez votre fichier Excel", type=['xls', 'xlsx']) # Espace pour ajouter le fichier excel qui sera traité
 
@@ -20,5 +17,8 @@ if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
 
     st.session_state.df = df
+    st.success("Document chargé !")
+
+
 
 
