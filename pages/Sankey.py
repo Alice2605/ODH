8a7816e2ef7ed7cf90329f84c578b346d8c917f9
Alice <77941134+Alice2605@users.noreply.github.com>
2023-12-22 14:27:16 +0100

    Update Sankey.py

--- pages/Sankey.py
+++ pages/Sankey.py
@@ -23,7 +23,8 @@ selected_option = st.selectbox('Sélectionnez un centre', options)
 
 # Si un centre est sélectionné dans le menu déroulant
 if selected_option in liste_hopitaux:
-    fig_centre_sankey, _ = donnees_centre(df, selected_option) 
+    df_trie_centre = donnees_centre(df, selected_option)
+    fig_centre_sankey, _ = fig_tout(df_trie_centre) 
 
     fig_centre_sankey.update_layout(
         #title_text="Patients ayant reçu du trastuzumab au centre", 
@@ -33,12 +34,13 @@ if selected_option in liste_hopitaux:
     fig_centre_sankey.update_traces(textfont_size=12) # Change la taille de police du texte des NOEUDS
     st.plotly_chart(fig_centre_sankey) # Affiche le graphique
 
-    st.download_button(label="Télécharger le graph au format html", data=fig_centre_sankey.to_html(full_html=False), file_name="Sankey_diagram.html")
+    st.download_button(label="Télécharger le diagramme au format html", data=fig_centre_sankey.to_html(full_html=False), file_name="Sankey_diagram.html")
 
 
 # Si TOUT sélectionné dans le menu déroulant (tous les centres)
 if selected_option == 'TOUT':
-    fig_tout_sankey, _ = donnees_tout(df)
+    df_trie_tout = donnees_tout(df)
+    fig_tout_sankey, _ = fig_tout(df_trie_tout)
 
     fig_tout_sankey.update_layout(
         title_text="Tous les patients ayant reçu du trastuzumab", 
@@ -48,8 +50,5 @@ if selected_option == 'TOUT':
     fig_tout_sankey.update_traces(textfont_size=12) # Change la taille de police du texte des NOEUDS
     st.plotly_chart(fig_tout_sankey) # Affiche le graphique
 
-    st.download_button(label="Télécharger le graph au format html", data=fig_tout_sankey.to_html(full_html=False), file_name="Sankey_diagram_tout.html")
-
-
-
+    st.download_button(label="Télécharger le diagramme au format html", data=fig_tout_sankey.to_html(full_html=False), file_name="Sankey_diagram_tout.html")
 
